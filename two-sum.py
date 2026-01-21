9d28bf1798d3a14a0b36cf52fb38082cab011b42
Madhu <madhuanandalli@gmail.com>
2026-01-21 12:38:43 +0530

    add leetcode solutions

---
+++ two-sum.py
@@ -0,0 +1,30 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```python []
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+               if nums[i] + nums[j] == target:
+                  return [i, j]
+
+```
