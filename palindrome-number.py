409493fb9a91cccde17efebc4a95e03a1dc30dc9
Madhu <madhuanandalli@gmail.com>
2026-01-21 13:00:34 +0530

    Add palindrome number solution

---
+++ palindrome-number.py
@@ -0,0 +1,32 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```python []
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        original = x
+        rev = 0
+
+        while x > 0:
+            rev = rev * 10 + x % 10
+            x //= 10 
+        return original == rev
+```
